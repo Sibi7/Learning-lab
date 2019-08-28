5ee11d9917a1a27a8f349be9cbc2ba1ebe8b035e
Dmitriy <apuc06@mail.ru>
2019-08-28 11:14:35 +0300

    section advantages

--- wp-content/themes/learning-lab/framework-customizations/extensions/shortcodes/shortcodes/advantages/options.php
+++ wp-content/themes/learning-lab/framework-customizations/extensions/shortcodes/shortcodes/advantages/options.php
@@ -5,10 +5,30 @@
  * Опции (поля) шорткода
  * @link Список всех возможных опицй http://manual.unyson.io/en/latest/options/built-in/introduction.html
  */
-//$options = [
-//    //ключ - slug опции, к которому будем обращаться во view
-//    //значение - массив конфигураций для опции
-//    'slug'     => [
-//        'param' => 'value',
-//    ]
-//];
+$options = [
+    //ключ - slug опции, к которому будем обращаться во view
+    //значение - массив конфигураций для опции
+    'list' => array(
+        'type' => 'addable-popup',
+        'label' => __('Услуги', '{domain}'),
+        'template' => '{{- title }}',
+        'size' => 'large', // small, medium, large
+        'add-button-text' => __('Добавить', '{domain}'),
+        'sortable' => true,
+        'popup-options' => array(
+            'title' => array(
+                'type' => 'text',
+                'label' => __('Заголовок', '{domain}'),
+            ),
+            'desk' => [
+                'type' => 'text',
+                'label' => 'Описание'
+            ],
+            'icon' => [
+                'type' => 'upload',
+                'label' => 'Иконка'
+            ]
+
+        ),
+    ),
+];
--- wp-content/themes/learning-lab/framework-customizations/extensions/shortcodes/shortcodes/advantages/views/view.php
+++ wp-content/themes/learning-lab/framework-customizations/extensions/shortcodes/shortcodes/advantages/views/view.php
@@ -1,5 +1,5 @@
-<?php if ( ! defined( 'FW' ) ) {
-    die( 'Forbidden' );
+<?php if (!defined('FW')) {
+    die('Forbidden');
 }
 /*
   * Верстка шорткода
@@ -8,57 +8,31 @@
 
 ?>
 
-<!--START SECTION ADVANTAGES-->
-<section id="section__advantages">
-    <div class="container">
-        <div class="row">
-            <!--START ADVANTAGES ITEM-->
-            <div class="col-md-4">
-                <div class="advantages__item">
-                    <div class="row">
-                        <div class="col-3 col-md-12 col-lg-3">
-                            <div class="advantages__item__icon advantages__item__icon--online"></div>
+    <!--START SECTION ADVANTAGES-->
+    <section id="section__advantages">
+        <div class="container">
+            <div class="row">
+                <?php if (is_array($atts['list']) && !empty($atts['list'])): ?>
+                    <?php foreach ($atts['list'] as $key => $att): ?>
+                        <div class="col-md-4">
+                            <div class="advantages__item">
+                                <div class="row">
+                                    <div class="col-3 col-md-12 col-lg-3">
+                                        <div class="advantages__item__icon"><img src="<?= $att['icon']['url']; ?>"
+                                                                                 alt="<?= $att['icon']['attachment_id']; ?>"></div>
+                                    </div>
+                                    <div class="col-9 col-md-12 col-lg-9 text-left text-md-center text-lg-left">
+                                        <div class="advantages__item__title"><?= $att['title']; ?></div>
+                                        <div class="advantages__item__text"><?= $att['desk']; ?></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-9 col-md-12 col-lg-9 text-left text-md-center text-lg-left">
-                            <div class="advantages__item__title">Обучение 100% онлайн</div>
-                            <div class="advantages__item__text">в любое время, в любом месте, с телефона или компьютера</div>
-                        </div>
-                    </div>
-                </div>
+
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </div>
-            <!--END ADVANTAGES ITEM-->
-            <!--START ADVANTAGES ITEM-->
-            <div class="col-md-4">
-                <div class="advantages__item">
-                    <div class="row">
-                        <div class="col-3 col-md-12 col-lg-3">
-                            <div class="advantages__item__icon advantages__item__icon--guarantee"></div>
-                        </div>
-                        <div class="col-9 col-md-12 col-lg-9 text-left text-md-center text-lg-left">
-                            <div class="advantages__item__title">Гарантия возврата денег</div>
-                            <div class="advantages__item__text">вернём вам деньги без вопросов, пока вы не начали изучать вторую тему</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--END ADVANTAGES ITEM-->
-            <!--START ADVANTAGES ITEM-->
-            <div class="col-md-4">
-                <div class="advantages__item">
-                    <div class="row">
-                        <div class="col-3 col-md-12 col-lg-3">
-                            <div class="advantages__item__icon advantages__item__icon--beginner"></div>
-                        </div>
-                        <div class="col-9 col-md-12 col-lg-9 text-left text-md-center text-lg-left">
-                            <div class="advantages__item__title">Уровень: для начинающих</div>
-                            <div class="advantages__item__text">наш тренинг подайдет  любому, т.к мы начинаем с самого простого</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--END ADVANTAGES ITEM-->
         </div>
-    </div>
-</section>
-<!--END SECTION ADVANTAGES-->
+    </section>
+    <!--END SECTION ADVANTAGES-->
 <?php fw_print($atts); ?>
